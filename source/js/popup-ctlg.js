412cc4b027eeece9e4f0f28454ed82af1273774c
f1eeman <mr.f1eeman@gmail.com>
2019-12-14 05:49:22 +0600

    Добавляет интерактивную карту и исправляет некоторые недочеты

--- source/js/popup-ctlg.js
+++ source/js/popup-ctlg.js
@@ -1,19 +1,15 @@
 var cartPopup = document.querySelector(".modal-cart");
 var cartOverlay = document.querySelector(".modal-overlay")
+var elements = document.querySelectorAll(".product__add-to-cart");
 
-var popupShow = function () {
-  var elements = document.querySelectorAll(".product__add-to-cart");
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    element.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.add("modal-cart__show");
-      cartOverlay.classList.add("modal-overlay__show");
-    });
-  }
+for (var i = 0; i < elements.length; i++) {
+  var element = elements[i];
+  element.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.add("modal-cart__show");
+  cartOverlay.classList.add("modal-overlay__show");
+  });
 };
-
-popupShow();
 
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27 && cartPopup.classList.contains("modal-cart__show")) {
